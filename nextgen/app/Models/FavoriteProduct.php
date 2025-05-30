df2a5393df3c5cb82d755d809a954aa4427813d1
Chau <chauttc01@gmail.com>
2025-05-30 10:52:59 +0700

    thêm favorite product và category

---
+++ nextgen/app/Models/FavoriteProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FavoriteProduct extends Model
+{
+    protected $fillable = [
+        'userid',
+        'productid',
+        'created_at'
+        ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
