ad4ed02dcd2189a0ce8f756ba8ace82943d28a04
truongdukiet <kiettdps41124@gmail.com>
2025-07-29 16:56:27 +0700

    chỉnh tiếng việt

--- frontend/src/layouts/MainLayout/ClientFooter.jsx
+++ frontend/src/layouts/MainLayout/ClientFooter.jsx
@@ -8,37 +8,37 @@ const ClientFooter = () => {
           <div className="col-lg-4 mb-5 mb-lg-0">
             <div className="row mb-5">
               <div className="col-md-12">
-                <h3 className="footer-heading mb-4">Navigation</h3>
+                <h3 className="footer-heading mb-4">Điều hướng</h3>
               </div>
               <div className="col-md-6 col-lg-6">
                 <ul className="list-unstyled">
                   <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/">Trang chủ</Link>
                   </li>
                   <li>
-                    <Link to="/services">Services</Link>
+                    <Link to="/services">Dịch vụ</Link>
                   </li>
                   <li>
-                    <Link to="/news">News</Link>
+                    <Link to="/news">Tin tức</Link>
                   </li>
                   <li>
-                    <a href="#">Team</a>
+                    <a href="#">Đội ngũ</a>
                   </li>
                 </ul>
               </div>
               <div className="col-md-6 col-lg-6">
                 <ul className="list-unstyled">
                   <li>
-                    <Link to="/about">About Us</Link>
+                    <Link to="/about">Về chúng tôi</Link>
                   </li>
                   <li>
-                    <a href="#">Privacy Policy</a>
+                    <a href="#">Chính sách bảo mật</a>
                   </li>
                   <li>
-                    <Link to="/contact">Contact Us</Link>
+                    <Link to="/contact">Liên hệ</Link>
                   </li>
                   <li>
-                    <a href="#">Membership</a>
+                    <a href="#">Thành viên</a>
                   </li>
                 </ul>
               </div>
@@ -46,7 +46,7 @@ const ClientFooter = () => {
           </div>
           <div className="col-lg-4">
             <div className="mb-5">
-              <h3 className="footer-heading mb-4">Recent News</h3>
+              <h3 className="footer-heading mb-4">Tin tức gần đây</h3>
               <div className="block-25">
                 <ul className="list-unstyled">
                   <li className="mb-3">
@@ -54,13 +54,13 @@ const ClientFooter = () => {
                       <figure className="image mr-4">
                         <img
                           src="/images/hero_bg_1.jpg"
-                          alt=""
+                          alt="Hình ảnh"
                           className="img-fluid"
                         />
                       </figure>
                       <div className="text">
                         <span className="small text-uppercase date">
-                          Sep 16, 2018
+                          Ngày 16 tháng 9, 2018
                         </span>
                         <h3 className="heading font-weight-light">
                           Lorem ipsum dolor sit amet consectetur elit
@@ -73,13 +73,13 @@ const ClientFooter = () => {
                       <figure className="image mr-4">
                         <img
                           src="/images/hero_bg_1.jpg"
-                          alt=""
+                          alt="Hình ảnh"
                           className="img-fluid"
                         />
                       </figure>
                       <div className="text">
                         <span className="small text-uppercase date">
-                          Sep 16, 2018
+                          Ngày 16 tháng 9, 2018
                         </span>
                         <h3 className="heading font-weight-light">
                           Lorem ipsum dolor sit amet consectetur elit
@@ -93,7 +93,7 @@ const ClientFooter = () => {
           </div>
           <div className="col-lg-4 mb-5 mb-lg-0">
             <div className="mb-5">
-              <h3 className="footer-heading mb-2">Subscribe Newsletter</h3>
+              <h3 className="footer-heading mb-2">Đăng ký nhận bản tin</h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit minima
                 minus odio.
@@ -103,8 +103,8 @@ const ClientFooter = () => {
                   <input
                     type="text"
                     className="form-control border-white text-white bg-transparent"
-                    placeholder="Enter Email"
-                    aria-label="Enter Email"
+                    placeholder="Nhập Email"
+                    aria-label="Nhập Email"
                     aria-describedby="button-addon2"
                   />
                   <div className="input-group-append">
@@ -113,7 +113,7 @@ const ClientFooter = () => {
                       type="button"
                       id="button-addon2"
                     >
-                      Send
+                      Gửi
                     </button>
                   </div>
                 </div>
@@ -121,7 +121,7 @@ const ClientFooter = () => {
             </div>
             <div className="row">
               <div className="col-md-12">
-                <h3 className="footer-heading mb-4">Follow Us</h3>
+                <h3 className="footer-heading mb-4">Theo dõi chúng tôi</h3>
                 <div>
                   <a href="#" className="pl-0 pr-3">
                     <span className="icon-facebook"></span>
@@ -143,8 +143,8 @@ const ClientFooter = () => {
         <div className="row pt-5 mt-5 text-center">
           <div className="col-md-12">
             <p>
-              Copyright &copy; {new Date().getFullYear()} All rights reserved |
-              NextGen Interior Design
+              Bản quyền &copy; {new Date().getFullYear()} Mọi quyền được bảo lưu |
+              Thiết kế Nội thất NextGen
             </p>
           </div>
         </div>
